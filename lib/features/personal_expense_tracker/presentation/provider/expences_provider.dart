6e91a55f7e6f79ae24abb803b062be7cbd8e110c
Esmail-Rahmani <60963312+Esmail-Rahmani@users.noreply.github.com>
2024-04-08 14:30:33 +0430

    database basic done

--- lib/features/personal_expense_tracker/presentation/provider/expences_provider.dart
+++ lib/features/personal_expense_tracker/presentation/provider/expences_provider.dart
@@ -1,21 +1,59 @@
-import 'dart:convert';
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
-import 'package:flutter/material.dart';
+import '../../data/models/expences.dart';
 
-class ExpensesProvider extends ChangeNotifier{
+class ExpenseProvider extends ChangeNotifier {
+  late Database _database;
 
-  List<String>? get categories => _categories;
-  List<String>?  _categories;
+  ExpenseProvider() {
+    _initDatabase();
+  }
 
-  setCategories(List<String> categoriesModel){
-    print("category models $categoriesModel");
-    _categories = categoriesModel;
+  Future<void> _initDatabase() async {
+    String path = join(await getDatabasesPath(), 'expense_tracker.db');
+    _database = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE expenses(id INTEGER PRIMARY KEY, amount REAL, description TEXT, date TEXT)',
+        );
+      },
+    );
+  }
+
+  Future<List<Expense>> getExpenses() async {
+    final List<Map<String, dynamic>> maps = await _database.query('expenses');
+    return List.generate(maps.length, (i) {
+      return Expense(
+        id: maps[i]['id'],
+        amount: maps[i]['amount'],
+        description: maps[i]['description'],
+        date: maps[i]['date'],
+      );
+    });
+  }
+
+  Future<void> addExpense(Expense expense) async {
+    await _database.insert(
+      'expenses',
+      expense.toJson(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
     notifyListeners();
   }
 
+  // Add other CRUD operations as needed
 
-  getCategories() async {
-
+  Future<void> deleteExpense(int id) async {
+    await _database.delete(
+      'expenses',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    notifyListeners();
   }
-
 }
